e563d6d6abfc1e59f46dfa8d52e55cb9ae77ecca
Michael Nelson <michael@nelsonware.com>
2019-05-23 14:39:02 -0700

    Add react app

---
+++ react-spa/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ProvidersIndex from './components/ProvidersIndex.js'
+
+function App() {
+  return (
+    <div className="App">
+      <ProvidersIndex></ProvidersIndex>
+    </div>
+  );
+}
+
+export default App;
