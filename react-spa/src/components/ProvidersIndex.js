a695de291d0829f76a13dce2b8ae24f26caf0d2b
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2019-07-08 19:22:12 -0700

    Display nicely formatted list

--- react-spa/src/components/ProvidersIndex.js
+++ react-spa/src/components/ProvidersIndex.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+
+import RateLine from './RateLine.js'
+
+const useApiFetch = () => {
+  const [rates, setRates] = useState([])
+  const [origins, setOrigins] = useState([])
+  const [destinations, setDestinations] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('http://localhost:3000/api')
+      res.json().then(res => {
+        setRates(res)
+        setOrigins([...new Set(res.map(r => r.origin))])
+        setDestinations([...new Set(res.map(r => r.destination))])
+      })
+    }
+    fetchData()
+  }, [])
+
+  return [rates, origins, destinations]
+}
 
 function ProvidersIndex() {
+  const [rates, origins, destinations] = useApiFetch()
+  const [originFilter, setOriginFilter] = useState('')
+  const [destFilter, setDestFilter] = useState('')
+
+  const filteredRates = rates.map(rate => (
+    <RateLine rate={rate} key={rate.id} />
+  ))
+
   return (
     <div className="ProvidersIndex">
       <h2>Providers</h2>
       <ul>
-        <li class="title">
+        <li className="title">
           <div>Company Name</div>
           <div>Total</div>
         </li>
+        {filteredRates}
       </ul>
     </div>
-  );
+  )
 }
 
-export default ProvidersIndex;
+export default ProvidersIndex
---
+++ react-spa/src/components/RateLine.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const USDFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+})
+
+export default function RateLine({ rate }) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: rate.amount_currency
+  })
+  return (
+    <li>
+      <div>{rate.name}</div>
+      {/* The spacing here leaves a lot to be desired! */}
+      <div>
+        {rate.origin} - {rate.destination}
+      </div>
+      <div>
+        {formatter.format(rate.amount_cents / 100)} (
+        {USDFormatter.format(rate.common_amount_cents / 100)})
+      </div>
+    </li>
+  )
+}
